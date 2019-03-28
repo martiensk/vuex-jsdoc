0930c5edcde96b3a9873d896eef14bc2a2840020
Martiens Kropff <martiensk@gmail.com>
2019-03-28 11:07:30 +0000

    Created a prototype action

--- index.js
+++ index.js
@@ -5,15 +5,18 @@
 
 const getterTag = require('./lib/getter');
 const mutatorTag = require('./lib/mutator');
+const actionTag = require('./lib/action');
 
 exports.defineTags = (dictionary) => {
     dictionary.defineTag(getterTag.name, getterTag.options);
     dictionary.defineTag(mutatorTag.name, mutatorTag.options);
+    dictionary.defineTag(actionTag.name, actionTag.options);
 };
 
 exports.handlers = {
     newDoclet(e) { /* eslint-disable-line */
         getterTag.newDocletHandler(e);
         mutatorTag.newDocletHandler(e);
+        actionTag.newDocletHandler(e);
     }
 };
