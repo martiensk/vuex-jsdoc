7d73adeffae3f95ec875cbab4ff04fb384d9a05c
Martiens Kropff <martiensk@gmail.com>
2019-03-28 10:29:50 +0000

    Created a module for the 'mutations' property

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
  */
 
 const getterTag = require('./lib/getter');
+const mutatorTag = require('./lib/mutator');
 
 exports.defineTags = (dictionary) => {
     dictionary.defineTag(getterTag.name, getterTag.options);
+    dictionary.defineTag(mutatorTag.name, mutatorTag.options);
 };
 
 exports.handlers = {
     newDoclet(e) { /* eslint-disable-line */
         getterTag.newDocletHandler(e);
+        mutatorTag.newDocletHandler(e);
     }
 };
