67c94c4e3c53eeb2d3725c1178d46dabadfd3123
Martiens Kropff <martiensk@gmail.com>
2019-03-28 09:27:00 +0000

    Fixed export of getterobject tag.

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
  */
 
 const getterTag = require('./lib/getter');
+const getterObjectTag = require('./lib/gettersObject');
 
 exports.defineTags = (dictionary) => {
     dictionary.defineTag(getterTag.name, getterTag.options);
+    dictionary.defineTag(getterObjectTag.name, getterObjectTag.options);
 };
 
 exports.handlers = {
     newDoclet(e) { /* eslint-disable-line */
         getterTag.newDocletHandler(e);
+        getterObjectTag.newDocletHandler(e);
     }
 };
