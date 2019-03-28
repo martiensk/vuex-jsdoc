b305f6fce449fe31e64e525ce0c7a988980dd128
Martiens Kropff <martiensk@gmail.com>
2019-03-28 09:44:38 +0000

    Fixed small template error, remove getters object property

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const getterObjectTag = require('./lib/gettersObject');
 
 exports.defineTags = (dictionary) => {
     dictionary.defineTag(getterTag.name, getterTag.options);
-    dictionary.defineTag(getterObjectTag.name, getterObjectTag.options);
 };
 
 exports.handlers = {
     newDoclet(e) { /* eslint-disable-line */
         getterTag.newDocletHandler(e);
-        getterObjectTag.newDocletHandler(e);
     }
 };
